d26b04251ff2588186356d9cd19ee22b42405cfc
jeetu <jitu.sehrawat>
2018-10-07 22:54:14 +0530

    signed in and stored to mlab

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const { mongodb } = require('./config/keys');
+
 const app = express();
 
 // set up view engine
 app.set('view engine','ejs');
+
+// connect to mongodb
+mongoose.connect(mongodb.dbURL, () => {
+  console.log(`Connected to DB`);
+})
 
 // setup routes
 app.use('/auth', authRoutes);
@@ -15,5 +23,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(3000, () => {
-  console.log(`app listening on port 3000`);
+  console.log(`App listening on port 3000`);
 });
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const { google } = require('./keys');
+const User = require('../models/user-model');
 
 passport.use(
   new GoogleStrategy({
@@ -10,7 +11,22 @@ passport.use(
     clientSecret: google.clientSecret
   }, (accessToken, refreshToken, profile, done) => {
     // passport callback func
-    console.log(`passport func called`);
-    console.log(profile);
+    // check if user already exists in our DB
+    User.findOne({
+      googleId: profile.id
+    }).then((currentUser) => {
+      if (currentUser) {
+        // already have the user
+        console.log(`User is: ${currentUser}`);
+      } else {
+        // create user in DB
+        new User({
+          username: profile.displayName,
+          googleId: profile.id
+        }).save().then((newUser) => {
+          console.log(`new user created: ${newUser}`);
+        })
+      }
+    })
   })
 )
